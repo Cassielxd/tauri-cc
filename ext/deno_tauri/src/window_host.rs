49c75e0f277cff72e536a3f192df073ab4cee5d9
李兴栋 <lixingdong@aliyun.com>
2024-01-12 17:55:25 +0800

    添加 deno 操作window

--- ext/deno_tauri/src/window_host.rs
+++ ext/deno_tauri/src/window_host.rs
@@ -24,12 +24,20 @@ deno_core::extension!(
 #[serde]
 fn create_window(state: &mut OpState, #[string] id: String, #[serde] url: WindowUrl) -> Result<(), AnyError> {
     let app_handle = state.borrow_mut::<AppHandle>();
-    match tauri::WindowBuilder::new(app_handle, id.clone(), url).build() {
-        Ok(win) => {
-            state.borrow_mut::<WindowsTable>().insert(id, TauriWindow { win });
+    //如果已经存在则不创建
+    if let Some(win) = app_handle.get_window(&*id.clone()) {
+        let windows = state.borrow_mut::<WindowsTable>();
+        if !windows.contains_key(&id) {
+            windows.insert(id, TauriWindow { win });
         }
-        Err(err) => {
-            println!("window creation failed: {}", err);
+    } else {
+        match tauri::WindowBuilder::new(app_handle, id.clone(), url).build() {
+            Ok(win) => {
+                state.borrow_mut::<WindowsTable>().insert(id, TauriWindow { win });
+            }
+            Err(err) => {
+                println!("window creation failed: {}", err);
+            }
         }
     }
     Ok(())
