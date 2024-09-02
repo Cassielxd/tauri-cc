e54084889c89a5fa3551a8e04b470f1be53c603a
李兴栋 <lixingdong@aliyun.com>
2024-09-02 19:42:01 +0800

    。。。。

--- src-tauri/resource/def.ts
+++ src-tauri/resource/def.ts
@@ -2,7 +2,8 @@ import Context from "./context.ts";
 import "./event.ts";
 
 await import("./db.ts");
-let ctx = new Context("./resource/");
+//打包的时候需要把替换成./resource/"
+let ctx = new Context("./src-tauri/resource/");
 ctx.start();
 dispatchEvent(new CustomEvent("started"));
 globalThis.applicationContext = ctx;
